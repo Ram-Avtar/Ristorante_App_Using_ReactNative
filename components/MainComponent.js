560423587936b8a7d322d88bd884fb2362a01f86
ram29avtar@gmail.com <ram29avtar@gmail.com>
2020-07-15 07:16:59 +0530

    navigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
+import {DISHES} from '../shared/dishes';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import Constants from 'expo-constants';
 import { createAppContainer } from 'react-navigation';
+import {createDrawerNavigator } from 'react-navigation-drawer';
+import { Icon } from 'react-native-elements';
 
 const MenuNavigator = createStackNavigator({
         Menu: { screen: Menu },
@@ -27,6 +30,43 @@ const MenuNavigator = createStackNavigator({
 
 export const MenuNavi = createAppContainer(MenuNavigator);
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+export const HomeNavi = createAppContainer(HomeNavigator);
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavi,
+        navigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home'
+        }
+      },
+    Menu: 
+      { screen: MenuNavi,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+export const MainNavi = createAppContainer(MainNavigator);
+
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -44,7 +84,7 @@ class Main extends Component {
  
     return (
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-            <MenuNavi />
+            <MainNavi />
         </View>
     );
   }
