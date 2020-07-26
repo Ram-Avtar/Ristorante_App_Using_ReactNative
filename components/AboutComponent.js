c7be3528cbec10e755d800f9f970efcfcfe729aa
ram29avtar@gmail.com <ram29avtar@gmail.com>
2020-07-26 18:31:30 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,8 @@ import { ListItem , Card} from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -17,28 +19,28 @@ function History(){
 	return(<Card
 				title='Our History'
 				>
-				<Text>Started in 2010, Ristorante con Fusion quickly established itself 
+				<Text>Started in 2010, Ristorante con Fusion quickly established itself
 				 as a culinary icon par excellence in Hong Kong. With its unique brand of
-				 world fusion cuisine that can be found nowhere else, it enjoys patronage 
-				 rom the A-list clientele in Hong Kong.  Featuring four of the best 
-				 three-star Michelin chefs in the world, you never know what will arrive 
+				 world fusion cuisine that can be found nowhere else, it enjoys patronage
+				 rom the A-list clientele in Hong Kong.  Featuring four of the best
+				 three-star Michelin chefs in the world, you never know what will arrive
 				 on your plate the next time you visit us.{"\n"}
 				 </Text>
 				<Text>
-				The restaurant traces its humble beginnings to The Frying Pan, a 
+				The restaurant traces its humble beginnings to The Frying Pan, a
 				successful chain started by our CEO, Mr. Peter Pan, that featured for the
 				 first time the world's best cuisines in a pan.
 				 </Text>
-				
+
 			</Card>)}
 class About extends Component{
 
 	static navigationOptions = {
-        title: 'About'
+        title: 'About Us'
     };
 	render(){
+		const {params}=this.props.navigation.state;
 		const renderLeaders = ({item, index}) => {
-
         		return (
 			                <ListItem
 			                    key={index}
@@ -50,23 +52,46 @@ class About extends Component{
 		        );
 		    };
 
-		const { navigate } = this.props.navigation;
 
-		return(
-		
-			<ScrollView style={{flex: 1}}>
-				<History />
-				<Card title='Corporate Leadership'>
-				<FlatList 
-	                data={this.props.leaders.leaders}
-	                renderItem={renderLeaders}
-	                keyExtractor={item => item.id.toString()}
-	                />
 
-                </Card>
-			</ScrollView>
-			);
+		if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                    <FlatList
+                        data={this.props.leaders.leaders}
+                        renderItem={renderLeaders}
+                        keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
 	}
 }
- 
+
 export default connect(mapStateToProps)(About);
