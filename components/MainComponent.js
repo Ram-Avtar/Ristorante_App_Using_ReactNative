677e0e8a0f4590fb9c11104e6857a9e064bcce4a
ram29avtar@gmail.com <ram29avtar@gmail.com>
2020-08-07 18:06:13 +0530

    favorite

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -13,6 +13,7 @@ import {createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
@@ -26,10 +27,27 @@ const mapDispatchToProps = dispatch => ({
   fetchPromos: () => dispatch(fetchPromos()),
   fetchLeaders: () => dispatch(fetchLeaders()),
 })
+const FavoriteNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
+})
+export const FavoriteNavi = createAppContainer(FavoriteNavigator);
 const ReservationNavigator = createStackNavigator({
     Reservation: { screen: Reservation }
   }, {
-    navigationOptions: ({ navigation }) => ({
+    defaultNavigationOptions: ({ navigation }) => ({
       headerStyle: {
           backgroundColor: "#512DA8"
       },
@@ -210,6 +228,21 @@ const MainNavigator = createDrawerNavigator({
             />
           )
         }, 
+      },
+      Favorites:
+      { screen: FavoriteNavi,
+        navigationOptions: {
+          title: 'My Favorite',
+          drawerLabel: 'My Favorite',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='heart'
+              type='font-awesome'            
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
       },
        Reservation:
       { screen: ReservationNavi,
